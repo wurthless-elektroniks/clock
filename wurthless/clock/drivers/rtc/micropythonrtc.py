506b3b4c0ba985266010dd0063985f958635c5eb
würthless elektroniks <wurthlesselektroniks@proton.me>
2023-11-19 18:16:18 -0500

    fix micropython clock running too slowly

--- wurthless/clock/drivers/rtc/micropythonrtc.py
+++ wurthless/clock/drivers/rtc/micropythonrtc.py
@@ -19,11 +19,11 @@ class MicropythonRTC(Rtc):
         rtcdt = self.rtc.datetime()
 
         # RTC does not work on the standard python time struct. we fix that here
-        pydt = ( rtcdt[0], rtcdt[1], rtcdt[2], rtcdt[4], rtcdt[5], 0, 0, 0, 0 )
+        pydt = ( rtcdt[0], rtcdt[1], rtcdt[2], rtcdt[4], rtcdt[5], rtcdt[6], 0, 0, 0 )
 
         return timeTupleToTimestamp(pydt)
     
     def setUtcTime(self, timestamp):
         pydt = timestampToTimeTuple(timestamp)
 
-        self.rtc.datetime( (pydt[0], pydt[1], pydt[2], 0, pydt[3], pydt[4], 0, 0) )
+        self.rtc.datetime( (pydt[0], pydt[1], pydt[2], 0, pydt[3], pydt[4], pydt[5], 0) )
