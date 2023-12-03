ecfa9a39d8b9485d51ac936a84f5ddc6b39c7fe7
würthless elektroniks <wurthlesselektroniks@proton.me>
2023-12-03 14:19:21 -0500

    make test shit happen faster

--- wurthless/clock/burnin.py
+++ wurthless/clock/burnin.py
@@ -10,25 +10,28 @@ def burnin(tot):
         tot.display().setDigitsBinary(0x7F, 0x7F, 0x7F, 0x7F)
         time.sleep(5)
 
-        for i in range(0,7):
-            dig = 1 << i
-            tot.display().setDigitsBinary(dig,dig,dig,dig)
-            time.sleep(5)
+        for j in range(0,10):
+            for i in range(0,7):
+                dig = 1 << i
+                tot.display().setDigitsBinary(dig,dig,dig,dig)
+                time.sleep(0.5)
 
-        tot.display().setDigitsBinary(0x7F, 0x00, 0x00, 0x00)
-        time.sleep(5)
-        tot.display().setDigitsBinary(0x00, 0x7F, 0x00, 0x00)
-        time.sleep(5)
-        tot.display().setDigitsBinary(0x00, 0x00, 0x7F, 0x00)
-        time.sleep(5)
-        tot.display().setDigitsBinary(0x00, 0x00, 0x00, 0x7F)
-        time.sleep(5)
+        for j in range(0,10):
+            tot.display().setDigitsBinary(0x7F, 0x00, 0x00, 0x00)
+            time.sleep(0.5)
+            tot.display().setDigitsBinary(0x00, 0x7F, 0x00, 0x00)
+            time.sleep(0.5)
+            tot.display().setDigitsBinary(0x00, 0x00, 0x7F, 0x00)
+            time.sleep(0.5)
+            tot.display().setDigitsBinary(0x00, 0x00, 0x00, 0x7F)
+            time.sleep(0.5)
 
         tot.display().setDigitsBinary(0x7F, 0x7F, 0x7F, 0x7F)
-        for i in range(0,8):
-            tot.display().setBrightness(8-i)
-            time.sleep(5)
-        for i in range(0,8):
-            tot.display().setBrightness(1+i)
-            time.sleep(5)
+        for j in range(0,10):
+            for i in range(0,8):
+                tot.display().setBrightness(8-i)
+                time.sleep(0.5)
+            for i in range(0,8):
+                tot.display().setBrightness(1+i)
+                time.sleep(0.5)
         
