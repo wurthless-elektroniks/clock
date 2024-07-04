0d2a5748eac55346bd691edfec9a45504eeff24d
würthless elektroniks <wurthlesselektroniks@proton.me>
2024-07-04 11:13:08 -0400

    add colors in curses driver

--- run_pc.py
+++ run_pc.py
@@ -49,6 +49,17 @@ def cursesMain(stdscr):
     # input driver requires it
     stdscr.nodelay(True)
 
+    if curses.can_change_color():
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)  
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)  
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_YELLOW)  
+        curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_BLUE)  
+        curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_MAGENTA)  
+        curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_CYAN)  
+        curses.init_pair(7, curses.COLOR_BLACK, curses.COLOR_WHITE)  
+        curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_GREEN) 
+        
     # platform initialization
     tot = ToT()
 
@@ -103,5 +114,5 @@ def main():
         return
 
     curses.wrapper(cursesMain)
-
+    
 main()
--- wurthless/clock/mock/cursesdisplay.py
+++ wurthless/clock/mock/cursesdisplay.py
@@ -17,47 +17,70 @@ def appendCharTab(bits, chars, tab):
         bit = (bits >> i) & 1
         tab[chars[i]] = LITE if bit == 1 else DARK
 
-def updateScreen(stdscr,a,b,c,d):
-    screenstr = u"""
-     aaaa   hhhh   oooo    vvvv
-    f    b m    i t    p  0    w
-    f    b m    i t    p  0    w
-     gggg   nnnn   uuuu    1111
-    e    c l    j s    q  z    x
-    e    c l    j s    q  z    x
-     dddd   kkkk   rrrr    yyyy
-    """
-
-    chartab = {}
-    appendCharTab(a, ['a','b','c','d','e','f','g'], chartab)
-    appendCharTab(b, ['h','i','j','k','l','m','n'], chartab)
-    appendCharTab(c, ['o','p','q','r','s','t','u'], chartab)
-    appendCharTab(d, ['v','w','x','y','z','0','1'], chartab)
-
-    x = 0
-    y = 0
-    
-    for c in screenstr:
-        if c == ' ':
-            x += 1
-            continue
-        elif c == '\n':
-            y += 1
-            x = 0
-        elif c == '\r':
-            x = 0
-        else:
-           stdscr.addch(y,x,chartab[c],curses.A_DIM if chartab[c] == DARK else curses.A_NORMAL)
-           x += 1
-
 class CursesDisplay(Display):
     def __init__(self, scr):
         self.scr = scr
+        self.brightness = 8
+        self.dig_a = 0
+        self.dig_b = 0
+        self.dig_c = 0
+        self.dig_d = 0
+
+    def _refresh(self):
+        screenstr = u"""
+             aaaa   hhhh   oooo    vvvv
+            f    b m    i t    p  0    w
+            f    b m    i t    p  0    w
+             gggg   nnnn   uuuu    1111
+            e    c l    j s    q  z    x
+            e    c l    j s    q  z    x
+             dddd   kkkk   rrrr    yyyy
+            """
+
+        self.scr.clear()
+        chartab = {}
+        appendCharTab(self.dig_a, ['a','b','c','d','e','f','g'], chartab)
+        appendCharTab(self.dig_b, ['h','i','j','k','l','m','n'], chartab)
+        appendCharTab(self.dig_c, ['o','p','q','r','s','t','u'], chartab)
+        appendCharTab(self.dig_d, ['v','w','x','y','z','0','1'], chartab)
+
+        color_tab = [ 0,
+                    curses.color_pair(1),
+                    curses.color_pair(2),
+                    curses.color_pair(3),
+                    curses.color_pair(4),
+                    curses.color_pair(5),
+                    curses.color_pair(6),
+                    curses.color_pair(7),
+                    curses.color_pair(8),
+                    ]
+        x = 0
+        y = 0
+        
+        for c in screenstr:
+            if c == ' ':
+                x += 1
+                continue
+            elif c == '\n':
+                y += 1
+                x = 0
+            elif c == '\r':
+                x = 0
+            else:
+                self.scr.addch(y,x,chartab[c], (curses.A_DIM if chartab[c] == DARK else (curses.A_NORMAL|color_tab[self.brightness])) )
+            x += 1
+
+        self.scr.refresh()
 
     def setBrightness(self, brightness):
-        pass
+        self.brightness = brightness
+        self._refresh()
 
     def setDigitsBinary(self,a,b,c,d):
-        updateScreen(self.scr,a,b,c,d)
-        self.scr.refresh()
+        self.dig_a = a
+        self.dig_b = b
+        self.dig_c = c
+        self.dig_d = d
+        self._refresh()
+
 
