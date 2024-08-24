4df64cae249da97a5a65c0e05b9c8ae8188f63ef
würthless elektroniks <wurthlesselektroniks@proton.me>
2024-08-24 11:17:54 -0400

    complete rewrite of webserver code

--- src/wurthless/clock/webserver/webserver.py
+++ src/wurthless/clock/webserver/webserver.py
@@ -1,15 +1,10 @@
 #
-# HTTP webserver stuff.
-#
-# GET /     - displays configuration form.
-# POST /sub - sends configuration data.
-#
-# Requires Microdot to run, though this dependency will probably be removed in the future
+# HTTP webserver and REST endpoint.
+# Requires Microdot to run.
 #
 
-from microdot import Microdot
+from microdot import Microdot,send_file
 from wurthless.clock.cvars.cvars import registerCvar
-
 
 global running_under_upy
 try:
@@ -26,59 +21,6 @@ registerCvar(u"wurthless.clock.webserver", u"server_active_pin", u"Int", u"I/O p
 # unfortunately, we have to store the ToT as a global...
 global g_tot
 
-utc_offsets = [
-    [ u"UTC-12:00", int(-12 * 3600) ],
-    [ u"UTC-11:00", int(-11 * 3600) ],
-    [ u"UTC-10:00", int(-10 * 3600) ],
-    [ u"UTC-09:30", int(-9.5 * 3600) ],
-    [ u"UTC-09:00", int(-9 * 3600) ],
-    [ u"UTC-08:00", int(-8 * 3600) ],
-    [ u"UTC-07:00", int(-7 * 3600) ],
-    [ u"UTC-06:00", int(-6 * 3600) ],
-    [ u"UTC-05:00", int(-5 * 3600) ],
-    [ u"UTC-04:00", int(-4 * 3600) ],
-    [ u"UTC-03:30", int(-3.5 * 3600) ],
-    [ u"UTC-03:00", int(-3 * 3600) ],
-    [ u"UTC-02:00", int(-2 * 3600) ],
-    [ u"UTC-01:00", int(-1 * 3600) ],
-    [ u"UTC",       int(0) ],
-    [ u"UTC+01:00", int(1 * 3600) ],
-    [ u"UTC+02:00", int(2 * 3600) ],
-    [ u"UTC+03:00", int(3 * 3600) ],
-    [ u"UTC+03:30", int(3.5 * 3600) ],
-    [ u"UTC+04:00", int(4 * 3600) ],
-    [ u"UTC+04:30", int(4.5 * 3600) ],
-    [ u"UTC+05:00", int(5 * 3600) ],
-    [ u"UTC+05:30", int(5.5 * 3600) ],
-    [ u"UTC+05:45", int(5.75 * 3600) ],
-    [ u"UTC+06:00", int(6 * 3600) ],
-    [ u"UTC+06:30", int(6.5 * 3600) ],
-    [ u"UTC+07:00", int(7 * 3600) ],
-    [ u"UTC+08:00", int(8 * 3600) ],
-    [ u"UTC+08:45", int(8.75 * 3600) ],
-    [ u"UTC+09:00", int(9 * 3600) ],
-    [ u"UTC+09:30", int(9.5 * 3600) ],
-    [ u"UTC+10:00", int(10 * 3600) ],
-    [ u"UTC+10:30", int(10.5 * 3600) ],
-    [ u"UTC+11:00", int(11 * 3600) ],
-    [ u"UTC+12:00", int(12 * 3600) ],
-    [ u"UTC+12:45", int(12.75 * 3600) ],
-    [ u"UTC+13:00", int(13 * 3600) ],
-    [ u"UTC+14:00", int(14 * 3600) ],
-]
-
-def generateUtcOffsetList():
-    items = []
-    current_timezone = g_tot.cvars().get(u"config.clock",u"utc_offset_seconds")
-
-    for i in utc_offsets:
-        if i[1] == current_timezone:
-            items.append(u"""<option value="%d" selected="selected">%s</option>"""%(i[1],i[0]))
-        else:
-            items.append(u"""<option value="%d">%s</option>"""%(i[1],i[0]))
-
-    return u"""<select name="utcoffset" id="utcoffset">"""+u"".join(items)+u"</select>"
-
 #
 # from https://www.cisco.com/assets/sol/sb/WAP321_Emulators/WAP321_Emulator_v1.0.0.3/help/Wireless05.html:
 # - string length must be 2-32 characters
@@ -89,7 +31,6 @@ def generateUtcOffsetList():
 def validateWifiAccessPointName(string):
     l = list(string)
     if ( 2 <= len(l) and len(l) <= 32) is False:
-        print(u"validateWifiAccessPointName(): too long")
         return False
 
     # also catches precisely one leading space
@@ -112,137 +53,79 @@ def validateWifiAccessPointName(string):
     
     return True
 
-def generateWifiAccessPoint():
-    ap_name = g_tot.cvars().get(u"config.nic",u"wifi_ap_name" )
-    if validateWifiAccessPointName(ap_name) is False:
-        ap_name = u""
-
-    return u"""<input type="text" id="ap_name" name="ap_name" value="%s"/>""" % (ap_name)
-
-def generateWifiPassword():
-    return u"""<input type="password" id="ap_password" name="ap_password"/>"""
-
-def generateDstSelection():
-    dst_disable = g_tot.cvars().get(u"config.clock",u"dst_disable")
-    dst_active = g_tot.cvars().get(u"config.clock",u"dst_active")
-
-    dst_off_item = u"""<input type="radio" id="dst_off" name="dst" value="off" checked />""" if dst_disable is False and dst_active is False else u"""<input type="radio" id="dst_off" name="dst" value="off" />"""
-    dst_on_item = u"""<input type="radio" id="dst_on" name="dst" value="on" checked />""" if dst_disable is False and dst_active is True else u"""<input type="radio" id="dst_on" name="dst" value="on" />"""
-    dst_disable_item = u"""<input type="radio" id="dst_disable" name="dst" value="disable" checked />""" if dst_disable is True else u"""<input type="radio" id="dst_disable" name="dst" value="disable" />"""
-
-    return u"""
-    %s
-    <label for="dst_off">off</label>
-    %s
-    <label for="dst_on">on</label>
-    %s
-    <label for="dst_disable">disable dst</label>
-    """%(dst_off_item,dst_on_item,dst_disable_item)
+@server.get('/index.html')
+async def indexhtml(request):
+    return send_file(u"www/index.html")
 
 @server.get('/')
 async def index(request):
-    return u"""
-    <html>
-    <head>
-    <title>the most useless clock in the world - config mode</title>
-    </head>
-    <body>
-    <h1>the most useless clock in the world - configuration mode</h1>
-    <p><i>presented in glorious 1998-era html because i am too lazy to write something modern</i></p>
-    <p>please enter details that i will be selling to russian hackers for $10 a pop:</p>
-    <form action="/sub" method="post">
-    <table>
-    <tr>
-    <td>wifi accesspoint</td>
-    <td>%s</td>
-    </tr>
-    <tr>
-    <td>wifi password</td>
-    <td>%s</td>
-    </tr>
-    <tr>
-    <td>timezone</td>
-    <td>%s</td>
-    </tr>
-    <tr>
-    <td>dst adjust</td>
-    <td>%s</td>
-    </tr>
-    </table>
-    <p><b>please note:</b> your wifi network must be accessible over the 2.4 GHz band. unfortunately, i can't provide a dropdown of available wifi networks because of technical limitations.</p>
-    <button action="submit">save settings</button>
-    </form>
-    </body>
-    </html>
-    """ % (generateWifiAccessPoint(),generateWifiPassword(),generateUtcOffsetList(),generateDstSelection()), 200, {'Content-Type': 'text/html'}
+    return send_file(u"www/index.html")
 
-@server.post('/sub')
-async def sub(request):
-    ap_name = request.form.get('ap_name')
-    ap_password = request.form.get('ap_password')
-    dst = request.form.get('dst')
-    utcoffset = request.form.get('utcoffset')
+@server.get('/cfg.js')
+async def index(request):
+    return send_file(u"www/cfg.js")
 
-    if ap_name is None or ap_name == u"" or ap_password is None or ap_password == u"" or dst is None or utcoffset is None:
-        return u"""
-        <html>
-        <head>
-        <title>oops</title>
-        </head>
-        <body>
-        <h1>required field not set</h1>
-        <p>all fields must be filled out. try again!!!</p>
-        </body>
-        </body>
-        </html>
-        """, 401, {'Content-Type': 'text/html'}
-    
-    # HTTP POST sucks and always replaces whitespace with + so handle that first.
-    ap_name = ap_name.replace("+", " ")
+@server.get('/cfg.css')
+async def index(request):
+    return send_file(u"www/cfg.css")
+
+@server.get('/rest/settings')
+async def settingsGet(request):
+    # do NOT send password back to the client, ever. EVER.
+    return {
+        'wifi_ap_name': g_tot.cvars().get(u"config.nic",u"wifi_ap_name"),
+        'dst_active': g_tot.cvars().get(u"config.clock",u"dst_active"),
+        'dst_disable': g_tot.cvars().get(u"config.clock",u"dst_disable"),
+        'utc_offset_seconds': g_tot.cvars().get(u"config.clock",u"utc_offset_seconds"),
+    },200
+
+@server.post('/rest/settings')
+async def settingsPost(request):
+    restdata = request.json
+    if restdata is None:
+        return {
+            'result': 'error',
+            'message': 'no-post-data'
+        },400
+
+    requiredFields = [ 'wifi_ap_name', 'wifi_ap_password', 'dst_active', 'dst_disable', 'utc_offset_seconds' ]
+
+    for f in requiredFields:
+        if f not in restdata:
+            return {
+                'result': 'error',
+                'message': 'missing-required-field'
+            }, 400
+
+    ap_name = restdata['wifi_ap_name']
+    ap_password = restdata['wifi_ap_password']
+    dst_active = restdata['dst_active']
+    dst_disable = restdata['dst_disable']
+    utc_offset_seconds = restdata['utc_offset_seconds']
 
     if validateWifiAccessPointName(ap_name) is False:
-        return u"""
-        <html>
-        <head>
-        <title>oops</title>
-        </head>
-        <body>
-        <h1>your wifi accesspoint name sucks</h1>
-        <p>according to <a href=https://www.cisco.com/assets/sol/sb/WAP321_Emulators/WAP321_Emulator_v1.0.0.3/help/Wireless05.html">this handy document</a>, your wifi accesspoint name is invalid. more specifically:<p>
-        <ul>
-        <li>the SSID must be between 2 and 32 characters long</li>
-        <li>it can't start or end with whitespace</li>
-        <li>it can't contain the characters: ?, ", $, [, \\, ], and +</li>
-        <li>it can't start with the characters: !, # and ;</li>
-        <li>it has to be ASCII characters only (whatever the hell that means, i don't know, i'm not a technically apt person)</li>
-        </ul>
-        <p>go back and fix it and maybe i'll let you by.</p>
-        <p>by the way, for debugging purposes, this is what you entered: %s</p>
-        </body>
-        </body>
-        </html>
-        """%(ap_name), 401, {'Content-Type': 'text/html'}
+        return {
+            'result': 'error',
+            'message': 'bad-ap-name'
+        }, 400
 
-    g_tot.cvars().set(u"config.nic",u"wifi_ap_name", str(ap_name) )
-    g_tot.cvars().set(u"config.nic",u"wifi_ap_password", str(ap_password) )
-    g_tot.cvars().set(u"config.clock",u"dst_disable", dst == u"disable")
-    g_tot.cvars().set(u"config.clock",u"dst_active", dst == u"on" )
-    g_tot.cvars().set(u"config.clock",u"utc_offset_seconds", int(utcoffset) )
+    # check datatypes
+    # dst_active/dst_disable must be booleans
+    # utc_offset_seconds must be int
+    # ap_name/ap_password must be strings
+
+    g_tot.cvars().set(u"config.nic",u"wifi_ap_name", ap_name )
+    g_tot.cvars().set(u"config.nic",u"wifi_ap_password", ap_password )
+    g_tot.cvars().set(u"config.clock",u"dst_disable", dst_disable)
+    g_tot.cvars().set(u"config.clock",u"dst_active", dst_active)
+    g_tot.cvars().set(u"config.clock",u"utc_offset_seconds", utc_offset_seconds)
 
     g_tot.cvars().save()
 
-    return u"""
-    <html>
-    <head>
-    <title>stuff happened</title>
-    </head>
-    <body>
-    <h1>hooray, settings saved successfully</h1>
-    <p>please reboot your clock by pressing RESET. if you need to reconnect to this config page, hold SET while pressing RESET.</p>
-    </body>
-    </body>
-    </html>
-    """, 200, {'Content-Type': 'text/html'}
+    return {
+        'result': 'success',
+        'message': 'settings-saved'
+    }, 200
 
 def serverMain(tot):
     tot.nic().initAsServer()
@@ -252,7 +135,6 @@ def serverMain(tot):
     # on ESP32 (probably other boards) where the display is driven through software
     # there will not be enough CPU time to update the display and handle server logic
     if tot.cvars().get("wurthless.clock.webserver", "disable_display_when_serving") is True:
-        print(u"shutting down display NOW")
         tot.display().shutdown()
     status_pin = tot.cvars().get("wurthless.clock.webserver", "server_active_pin")
     if status_pin != -1 and running_under_upy is True:
---
+++ www/cfg.js
@@ -0,0 +1,179 @@
+//
+// configuration panel JS handling, all done in vanilla js to keep it easier.
+//
+var timezones = [
+    { value: -12*3600,  label: "UTC-12:00" },
+    { value: -11*3600,  label: "UTC-11:00" },
+    { value: -10*3600,  label: "UTC-10:00" },
+    { value: -9.5*3600, label: "UTC-9:30"  },
+    { value: -9*3600,   label: "UTC-9:00"  },
+    { value: -8*3600,   label: "UTC-8:00"  },
+    { value: -7*3600,   label: "UTC-7:00"  },
+    { value: -6*3600,   label: "UTC-6:00"  },
+    { value: -5*3600,   label: "UTC-5:00"  },
+    { value: -4*3600,   label: "UTC-4:00"  },
+    { value: -3*3600,   label: "UTC-3:30"  },
+    { value: -3*3600,   label: "UTC-3:00"  },
+    { value: -2*3600,   label: "UTC-2:00"  },
+    { value: -1*3600,   label: "UTC-1:00"  },
+    { value: 0,         label: "UTC"       },
+    { value: 1    * 3600,  label: "UTC+01:00" },
+    { value: 2    * 3600,  label: "UTC+02:00" },
+    { value: 3    * 3600,  label: "UTC+03:00" },
+    { value: 3.5  * 3600,  label: "UTC+03:30" },
+    { value: 4    * 3600,  label: "UTC+04:00" },
+    { value: 5    * 3600,  label: "UTC+05:00" },
+    { value: 5.5  * 3600,  label: "UTC+05:30" },
+    { value: 5.75 * 3600,  label: "UTC+05:45" },
+    { value: 6    * 3600,  label: "UTC+06:00" },
+    { value: 6.5  * 3600,  label: "UTC+06:30" },
+    { value: 7    * 3600,  label: "UTC+07:00" },
+    { value: 8    * 3600,  label: "UTC+08:00" },
+    { value: 8.75 * 3600,  label: "UTC+08:45" },
+    { value: 9    * 3600,  label: "UTC+09:00" },
+    { value: 9.5  * 3600,  label: "UTC+09:30" },
+    { value: 10   * 3600,  label: "UTC+10:00" },
+    { value: 10.5 * 3600,  label: "UTC+10:30" },
+    { value: 11   * 3600,  label: "UTC+11:00" },
+    { value: 12   * 3600,  label: "UTC+12:00" },
+    { value: 12.75 * 3600,  label: "UTC+12:45" },
+    { value: 13   * 3600,  label: "UTC+13:00" },
+    { value: 14   * 3600,  label: "UTC+14:00" }
+];
+
+function initTimeZones() {
+    let utcoffsetSelector = document.getElementById("utcoffset");
+    for (var tz in timezones) {
+        let opt = document.createElement("option");
+        opt.text = timezones[tz].label;
+        opt.value = timezones[tz].value;
+        utcoffsetSelector.add(opt);
+    } 
+}
+
+//
+// from https://www.cisco.com/assets/sol/sb/WAP321_Emulators/WAP321_Emulator_v1.0.0.3/help/Wireless05.html:
+// - string length must be 2-32 characters
+// - allowable characters are: ASCII 0x20, 0x21, 0x23, 0x25 through 0x2A, 0x2C through 0x3E, 0x40 through 0x5A, 0x5E through 0x7E. 
+// - trailing and leading spaces are not permitted
+// - string cannot start with 0x21, 0x23, 0x3B
+//
+function validateAccessPointName(apname) {
+    if (!(2 <= apname.length && apname.length <= 32)) {
+        return false;
+    }
+    
+    if ( [ 0x21, 0x23, 0x3B, 0x20 ].includes(apname.charCodeAt(0)) ) {
+        return false;
+    }
+
+    for (var i = 0; i < apname.length; i++) {
+        let ch = apname.charCodeAt(i);
+        if ( !(
+              ch == 0x20 ||
+              ch == 0x21 ||
+              ch == 0x23 ||
+              (0x25 <= ch && ch <= 0x2A) ||
+              (0x2C <= ch && ch <= 0x3E) ||
+              (0x40 <= ch && ch <= 0x5A) ||
+              (0x5E <= ch && ch <= 0x7E)
+            )
+        ) {
+            return false;
+        }
+    }
+
+    // catch trailing space (shouldn't get out of bounds error, length was already checkeds)
+    return apname.charCodeAt(apname.length - 1) != 0x20;
+}
+
+function validateForm() {
+    if (!validateAccessPointName(document.getElementById("ap_name").value)) {
+        return false;
+    }
+
+    return true;
+}
+
+function changeDstSelection(which) {
+    document.getElementById("dst_off").className = "butane";
+    document.getElementById("dst_on").className = "butane";
+    document.getElementById("dst_disable").className = "butane";
+
+    document.getElementById(which).className = "butaneselectad";
+}
+
+function selectDstOff() {
+    changeDstSelection("dst_off");
+}
+
+function selectDstOn() {
+    changeDstSelection("dst_on");
+}
+
+function selectDstDisabled() {
+    changeDstSelection("dst_disable");
+}
+
+function submitForm() {
+    let restreq = {
+        'wifi_ap_name':     document.getElementById("ap_name").value,
+        'wifi_ap_password': document.getElementById("ap_password").value,
+        'utc_offset_seconds': document.getElementById("utcoffset").value,
+        'dst_active': document.getElementById("dst_on").className === "butaneselectad",
+        'dst_disable': document.getElementById("dst_disable").className === "butaneselectad"
+    };
+
+    fetch("/rest/settings",{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(restreq)
+    }).then( (response) => response.json() )
+    .then( (response) => {
+        document.getElementById("success").style.display = "";
+        document.getElementById("form").remove();
+    })
+    .catch( (error) => {
+        console.log(error);
+    });
+}
+
+function populateSettings() {
+    fetch("/rest/settings")
+        .then( (response) => response.json() )
+        .then( (json) =>  {
+            document.getElementById("ap_name").value = json['wifi_ap_name'];
+            document.getElementById("utcoffset").value = json['utc_offset_seconds'];
+
+            if (json['dst_disable'] === true) {
+                selectDstDisabled();
+            } else if (json['dst_active'] === true) {
+                selectDstOn();
+            } else {
+                selectDstOff();
+            }
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("document load done.");
+
+    populateSettings();
+
+    var apname = document.getElementById("ap_name");
+    apname.addEventListener('change', function(e) {
+        this.setCustomValidity('');
+        if (!this.checkValidity()) {
+            return;
+        }
+
+        if (!validateAccessPointName(apname.value)) {
+            this.setCustomValidity('Invalid SSID format');
+        }
+    });
+
+    document.getElementById("form").style.visibility = "visible";
+    initTimeZones();
+});
