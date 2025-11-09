18299ac47a1d3cbed87ac1c92d4b4fa7e49c503d
würthless elektroniks <wurthlesselektroniks@proton.me>
2025-11-09 10:13:46 -0500

    ivl2-7/5: fix ghosting bug

--- src/wurthless/clock/drivers/display/rp2040displayivl.py
+++ src/wurthless/clock/drivers/display/rp2040displayivl.py
@@ -15,7 +15,7 @@ DIGIT_DRIVE_BASE_PIN = 11 # + 12, 13, 14, 15
 SEGMENT_DRIVE_BASE_PIN = 16 # + 17, 18, 19, 20, 21, 22
 PWM_PIN = 10
 
-registerCvar(u"wurthless.clock.drivers.display.rp2040display",
+registerCvar(u"wurthless.clock.drivers.display.rp2040displayivl",
              u"low_power_drives",
              u"Boolean",
              u"If true, reduces current drive power on the segment and digit control GPIOs. Default is True.",
@@ -52,14 +52,24 @@ def sevseg():
     label("0")
     pull(noblock)           .side(0)      # get whatever the CPU wants us to display
     mov(x, osr)             .side(0)      # push to output shift register (all values 8 bits, msb unused)
-    out(pins, 8)            .side(1)      # display digit 3 (display is rendered in reverse)
+    
+    # digits have to be strobed this way or else there will be ghosting
+    out(pins, 8)            .side(0)      # clock out bits while digit disabled
+    nop()                   .side(1)      # enable the digit
+    set(pins, 0)            .side(16)     # clear pins, enable colon segment
+
+    out(pins, 8)            .side(0)
+    nop()                   .side(2)
+    set(pins, 0)            .side(16)
+
+    out(pins, 8)            .side(0)
+    nop()                   .side(4)
     set(pins, 0).side(16)
-    out(pins, 8)            .side(2)      # display digit 2
-    set(pins, 0).side(16)
-    out(pins, 8)            .side(4)      # display digit 1
-    set(pins, 0).side(16)
-    out(pins, 8)            .side(8)      # display digit 0
-    set(pins, 0).side(16)
+    
+    out(pins, 8)            .side(0)
+    nop()                   .side(8)
+    set(pins, 0)            .side(16)
+    
     jmp("0")                .side(0)      # blank display and repeat
     wrap()
 
@@ -71,7 +81,7 @@ class Rp2040IvlDisplay(SevenSegmentDisplay):
 
         # attempt to grab I/Os here by setting them all as outputs
         # it is better to have them setup in a known state than to assume the statemachine will do everything for us
-        low_power_drives     = tot.cvars().get(u"wurthless.clock.drivers.display.rp2040display", u"low_power_drives")   
+        low_power_drives     = tot.cvars().get(u"wurthless.clock.drivers.display.rp2040displayivl", u"low_power_drives")   
         for i in range(segment_drive_base_pin, segment_drive_base_pin+7):
             Pin(i, Pin.OUT)
             if low_power_drives is True:
